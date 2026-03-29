630533ed20586e11bb20da1f65553723f6fd37b8
miseon768-tech <miseon768@gmail.com>
2026-03-29 22:03:15 +0900

    practice.py에 애완동물 소개 출력 기능 추가

--- practice.py
+++ practice.py
@@ -28,15 +28,19 @@ print(not (5>10))
 # 변수
 # 애완동물을 소개해 주세요~
 
-animal = "강아지"
-name = "연탄이"
+animal = "고양이"
+name = "해피"
 age = 4
-hobby = "산책"
+hobby = "낮잠"
 is_adult = age >= 3
 
+'''이렇게 하면 주석처리 됩니다'''
+'''control / 하면 여러문장 주석처리'''
 
 print("우리집 "+animal+"의 이름은 "+name+"예요")
-print(name+"은(는) "+str(age)+"살이며, "+hobby+"을(를) 아주 좋아해요")
+hobby = "공놀이"
+# print(name+"은(는) "+str(age)+"살이며, "+hobby+"을(를) 아주 좋아해요")
+print(name, "은(는) ", age, "살이며, " ,hobby, "을(를) 아주 좋아해요")
 print(name+"은(는) 어른일까요? "+str(is_adult))
 
 
